61384115aee432be82cfe37a814c1f33437635c0
leolpaz <leonardolpaz95@gmail.com>
2022-01-31 12:22:09 -0300

    Create controllers and view skeletons

---
+++ app/controllers/recipe_controller.rb
@@ -0,0 +1,16 @@
+class RecipeController < ApplicationController
+  def index
+  end
+  
+  def show
+  end
+
+  def create
+  end
+
+  def public_recipes
+  end
+
+  def destroy
+  end
+end
--- app/controllers/users_controller.rb
+++
@@ -1,5 +0,0 @@
-class UsersController < ApplicationController
-  before_action :authenticate_user!
-  def index
-  end
-end
