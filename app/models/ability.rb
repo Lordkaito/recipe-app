1f183d4b6ffc47069950a144f3686a6caae5dfb3
leolpaz <leonardolpaz95@gmail.com>
2022-02-01 20:05:44 -0300

    Add public recipe list and authorizations

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, Recipe, public: true
     return unless user.present?
 
     can :manage, Recipe, user_id: user.id
-    can :create, Recipe
+    can :manage, RecipeFood, user_id: user.id
   end
 end
